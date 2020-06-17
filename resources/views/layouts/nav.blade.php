48702721fef2adca7206ccf81070327c2486acb0
Merlijn Busch <24628@ma-web.nl>
2020-06-17 13:47:04 +0200

    updated navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,29 @@
-<nav>
+<nav class="nav-container">
     <div>
         logo
     </div>
 
-    <div>
-        <div>sign up</div>
-        <div>login</div>
+    <div class="nav-action-container">
+        @guest
+            <a class="btn"
+               href="{{ route('login') }}">{{ __('Login') }}</a>
+            @if (Route::has('register'))
+                <a class="btn"
+                   href="{{ route('register') }}">{{ __('Register') }}</a>
+            @endif
+        @else
+            <a class="btn"
+               href="{{ route('logout') }}"
+               onclick="event.preventDefault(); document.getElementById('logout-form-nav').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form-nav"
+                  action="{{ route('logout') }}"
+                  method="POST>
+                @csrf
+            </form>
+        @endguest
         <div>search</div>
         <div>hambuger icon</div>
     </div>
