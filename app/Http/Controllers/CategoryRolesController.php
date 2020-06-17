885750d45b0b42ca32f57c97ad9c462828c1205e
Merlijn Busch <24628@ma-web.nl>
2020-06-17 14:08:42 +0200

    added more mdoels

---
+++ app/Http/Controllers/CategoryRolesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CategoryRoles;
+use Illuminate\Http\Request;
+
+class CategoryRolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CategoryRoles  $categoryRoles
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CategoryRoles $categoryRoles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CategoryRoles  $categoryRoles
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CategoryRoles $categoryRoles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CategoryRoles  $categoryRoles
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CategoryRoles $categoryRoles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CategoryRoles  $categoryRoles
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CategoryRoles $categoryRoles)
+    {
+        //
+    }
+}
